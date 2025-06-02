da63cc3014a6950d85523f910d96f64f79cf55b5
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-06-02 19:22:30 +0300

    45

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:es28/routes.dart';
-import 'package:es28/veiw/screen/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
--- lib/veiw/screen/home/althakr/althakr.dart
+++ lib/veiw/screen/home/althakr/althakr.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import '../../../../controller/elathaker_controller.dart';
-import '../../../../core/constant/color.dart';
 
 class Althakr extends StatelessWidget {
   const Althakr({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Column(
+   return const Column(
      children: [
        // MaterialButton(onPressed:() =>Get.to(page) ,child: ,)
      ],
