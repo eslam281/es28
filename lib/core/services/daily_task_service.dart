090a07277eb469f04a30ad4177f617c4979d232e
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-07-26 00:27:38 +0300

    background test 11

--- lib/core/services/daily_task_service.dart
+++ lib/core/services/daily_task_service.dart
@@ -1,43 +1,6 @@
-// import 'package:workmanager/workmanager.dart';
-// import '../services/time_service.dart';
-//
-// const String dailyTask = "dailyPrayerTimeTask";
-//
-// /// الدالة التي ينفذها WorkManager
-// void callbackDispatcher() {
-//   Workmanager().executeTask((task, inputData) async {
-//     if (task == dailyTask) {
-//       await times(false, true);
-//     }
-//     return Future.value(true);
-//   });
-// }
-//
-// /// تسجيل مهمة يومية تبدأ عند منتصف الليل
-// Future<void> setupDailyTask() async {
-//   await Workmanager().initialize(
-//     callbackDispatcher,
-//     isInDebugMode: false,
-//   );
-//
-//   await Workmanager().registerPeriodicTask(
-//     "1", // unique ID
-//     dailyTask,
-//     frequency: const Duration(hours: 24),
-//     initialDelay: _calculateInitialDelayUntilMidnight(),
-//     constraints: Constraints(
-//       networkType: NetworkType.connected,
-//     ),
-//   );
-// }
-//
-// /// حساب الفرق حتى منتصف الليل
-// Duration _calculateInitialDelayUntilMidnight() {
-//   final now = DateTime.now();
-//   final tomorrow = DateTime(now.year, now.month, now.day + 1);
-//   return tomorrow.difference(now);
-// }
+
 import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 import 'package:workmanager/workmanager.dart';
 import '../../main.dart';
 import '../services/time_service.dart';
@@ -45,6 +8,17 @@ import '../services/time_service.dart';
 const String dailyTask = "dailyPrayerTimeTask";
 
 Future<void> setupDailyTask() async {
+
+  if (!Hive.isBoxOpen("times")) {
+    Hive.init("/data/user/0/com.example.es28/app_flutter"); // ✅ المسار الثابت
+    myBox =await Hive.openBox("times");
+  }
+
+  bool shouldDelayUntilMidnight = await checkDate();
+  Duration delay = shouldDelayUntilMidnight
+      ? _calculateInitialDelayUntilMidnight()
+      : const Duration(seconds: 10);
+
   await Workmanager().initialize(
     callbackDispatcher,
     isInDebugMode: true, // للتجربة السريعة
@@ -54,26 +28,30 @@ Future<void> setupDailyTask() async {
     "1", // unique ID
     dailyTask,
     frequency: const Duration(hours: 1),
-    initialDelay: const Duration(seconds:10),
+    initialDelay:delay,
     constraints: Constraints(
       networkType: NetworkType.connected,
     ),
   );
 }
 
-
-
 @pragma('vm:entry-point')
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     // if (task == dailyTask) {
-    if (!Hive.isBoxOpen("times")) {
-      Hive.init("/data/user/0/com.example.es28/app_flutter"); // ✅ المسار الثابت
-    myBox =await Hive.openBox("times");
-    }
       print("✅✅✅ WorkManager task executed ✅✅✅");
       await times(false, true);
     // }
     return Future.value(true);
   });
+}
+/// حساب الفرق حتى منتصف الليل
+Duration _calculateInitialDelayUntilMidnight() {
+  final now = DateTime.now();
+  final tomorrow = DateTime(now.year, now.month, now.day + 1);
+  return tomorrow.difference(now);
+}
+Future<bool> checkDate()async{
+  String dateResponse =await myBox?.get("date")??"";
+  return dateResponse==DateFormat('dd-MM-yyyy').format(DateTime.now());
 }
