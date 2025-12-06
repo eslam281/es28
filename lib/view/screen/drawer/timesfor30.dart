dde838ba4c5d4d41947df3ac9c82a74600133772
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-12-06 16:49:01 +0200

    some fix in drawer time for 30

--- lib/view/screen/drawer/timesfor30.dart
+++ lib/view/screen/drawer/timesfor30.dart
@@ -26,7 +26,7 @@ class Timesfor30Widget extends StatelessWidget {
         return HandlingDataView(
           statusRequest: controller.statusRequest,
           widget: Padding(
-            padding: const EdgeInsets.all(12.0),
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
             child: Column(
               children: [
                 /// Header: التاريخ + الموقع + تحديث
@@ -47,50 +47,86 @@ class Timesfor30Widget extends StatelessWidget {
                         ? const ShimmerReload()
                         : SingleChildScrollView(
                       scrollDirection: Axis.horizontal, // يسمح بالتمرير لو الجدول كبير
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: DataTable(
-                          headingTextStyle:const TextStyle(color: AppColor.secondColor,fontSize: 16,fontWeight: FontWeight.bold) ,
-                          columnSpacing: 20,
-                          dataRowMinHeight: 30,
-                          dataRowMaxHeight: 40,
-                          dividerThickness: .2,
-                          columns: const [
-                            DataColumn(label: Text("اليوم")),
-                            DataColumn(label: Text("الفجر")),
-                            DataColumn(label: Text("الشروق")),
-                            DataColumn(label: Text("الظهر")),
-                            DataColumn(label: Text("العصر")),
-                            DataColumn(label: Text("المغرب")),
-                            DataColumn(label: Text("العشاء")),
-                            DataColumn(label: Column(
-                              children: [
-                                Text("الثلث الأخير"),
-                                Text("من الليل"),
-                              ],)
+                      child: Column(
+                        children: [
+                          DataTable(
+                            headingTextStyle: const TextStyle(
+                              color: AppColor.secondColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
-                          rows: List.generate(
-                            controller.data!.length,
-                                (index) {
-                              final item = controller.data![index];
-                              return DataRow(
-                                color: index+1==DateTime.now().day?
-                                WidgetStateProperty.all(AppColor.primaryColor):null,
-                                cells: [
-                                  DataCell(Text("${index + 1}")),
-                                  DataCell(Text(item.fajr?.substring(0, 5) ?? "")),
-                                  DataCell(Text(item.sunrise?.substring(0, 5) ?? "")),
-                                  DataCell(Text(convertF(item.dhuhr!))),
-                                  DataCell(Text(convertF(item.asr!))),
-                                  DataCell(Text(convertF(item.maghrib!))),
-                                  DataCell(Text(convertF(item.isha!))),
-                                  DataCell(Center(child: Text(item.lastthird?.substring(0, 5) ?? ""))),
-                                ],
-                              );
-                            },
+                            columnSpacing: 0,
+                            columns: const [
+                              DataColumn(label: SizedBox(width: 30, child: Center(child: Text("اليوم")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("الفجر")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("الشروق")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("الظهر")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("العصر")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("المغرب")))),
+                              DataColumn(label: SizedBox(width: 70, child: Center(child: Text("العشاء")))),
+                              DataColumn(
+                                label: SizedBox(
+                                  width: 90,
+                                  child: Center(
+                                    child: Column(
+                                      children: [
+                                        Text("الثلث الأخير"),
+                                        Text("من الليل"),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                            rows: const [],
                           ),
-                        ),
+
+
+
+                          Expanded(
+                            child: SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              child: DataTable(
+                                headingRowHeight: 0,
+                                columnSpacing: 0,
+                                dataRowMinHeight: 30,
+                                dataRowMaxHeight: 40,
+                                dividerThickness: .2,
+                                columns: const [
+                                  DataColumn(label: SizedBox()), // هيدر مخفي
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                  DataColumn(label: SizedBox()),
+                                ],
+                                rows: List.generate(
+                                  controller.data!.length,
+                                      (index) {
+                                    final item = controller.data![index];
+                                    return DataRow(
+                                      color: index+1==DateTime.now().day?
+                                      WidgetStateProperty.all(AppColor.primaryColor):null,
+                                      cells: [
+                                        DataCell(SizedBox(width: 30, child: Text("${index + 1}"),),),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(item.fajr?.substring(0, 5) ?? "")))),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(item.sunrise?.substring(0, 5) ?? "")))),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(convertF(item.dhuhr!))))),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(convertF(item.asr!))))),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(convertF(item.maghrib!))))),
+                                        DataCell(SizedBox(width: 70, child: Center(child: Text(convertF(item.isha!))))),
+                                        DataCell(SizedBox(width: 90, child: Center(child: Text(item.lastthird?.substring(0, 5) ?? "")))),
+
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
